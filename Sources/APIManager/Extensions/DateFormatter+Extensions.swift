df6f10e9e5b4479bbc955fabb1632ef17d2a6808
Betül Çalık <betulcalik82@icloud.com>
2025-05-11 21:25:43 +0300

    dateformatter fix

--- Sources/APIManager/Extensions/DateFormatter+Extensions.swift
+++ Sources/APIManager/Extensions/DateFormatter+Extensions.swift
@@ -12,7 +12,8 @@ extension DateFormatter {
     static let backendFormat: DateFormatter = {
         let dateFormatter = DateFormatter()
         dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
         return dateFormatter
     }()
     
